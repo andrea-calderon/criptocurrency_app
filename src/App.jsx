765dfa3b10a9c0d60a473256792e799cc10c3ba9
Andrea Alvanés <alvanesc@gmail.com>
2025-01-06 10:09:51 -0600

    feat: add crypto list, integrate axios and include CSS file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+
 function App() {
+  const API_URL = import.meta.env.VITE_API_URL;
+  const [cryptos, setCryptos] = useState();
+
+  useEffect(() => {
+    axios
+      .get(`${API_URL}`)
+      .then((data) => {
+        setCryptos(data.data.data);
+      })
+      .catch(() => {
+        console.error("la peticion fallo");
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (!cryptos) return <span>Loading...</span>;
+
   return (
     <>
       <h2>Cryptocurrency real-time activity </h2>
+
+      <ol>
+        {cryptos.map(({ id, name, priceUsd }) => (
+          <li key={id}>
+            Nombre: {name} Precio: {priceUsd}
+          </li>
+        ))}
+      </ol>
     </>
   );
 }
